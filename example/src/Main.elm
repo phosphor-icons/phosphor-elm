81ba496d7edce6aeaf9b2f1a298b5041c7448bca
rektdeckard <friedtm@gmail.com>
2022-02-04 11:43:30 -0500

    feat(example): polish example app

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -1,10 +1,11 @@
-module Example exposing (..)
+module Main exposing (..)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-import Phosphor exposing (IconWeight(..), toHtml, withSize, withSizeUnit)
+import Html.Events exposing (onInput, onClick)
+import Html.Events.Extra.Wheel as Wheel
+import Phosphor exposing (IconWeight(..), toHtml, withSize, withSizeUnit, withClass)
 
 
 
@@ -37,6 +38,8 @@ init =
 type Msg
     = SetWeight String
     | SetSize String
+    | Scrolling Float
+    | Reset
     | NoOp
 
 
@@ -47,7 +50,13 @@ update msg model =
             { model | weight = parseWeight weight }
 
         SetSize size ->
-            { model | size = size |> String.toFloat |> Maybe.withDefault 128 }
+            { model | size = clamp 4 2000 (size |> String.toFloat |> Maybe.withDefault 128) }
+
+        Scrolling deltaY ->
+            { model | size = clamp 4 2000 (model.size - (deltaY * 0.2)) }
+
+        Reset ->
+            init
 
         _ ->
             model
@@ -65,9 +74,10 @@ view model =
             [ style "display" "grid"
             , style "place-content" "center"
             , style "height" "100vh"
+            , Wheel.onWheel (\event -> Scrolling event.deltaY)
             ]
             [ Html.node "style" [] [ text css ]
-            , div
+              , h2
                 [ style "position" "fixed"
                 , style "top" "0"
                 , style "left" "0"
@@ -79,7 +89,23 @@ view model =
                 , style "align-items" "center"
                 , style "padding" "16px"
                 ]
-                [ select [ onInput SetWeight ]
+                [ Phosphor.phosphorLogo Fill |> withSize 4 |> toHtml []
+                , text "phosphor-elm"
+                ]
+            , div
+                [ style "position" "fixed"
+                , style "bottom" "0"
+                , style "left" "0"
+                , style "right" "0"
+                , style "z-index" "1"
+                , style "display" "flex"
+                , style "gap" "16px"
+                , style "justify-content" "center"
+                , style "align-items" "center"
+                , style "padding" "16px"
+                , class "grid"
+                ]
+                [ select [ onInput SetWeight, style "height" "21px" ]
                     [ option [ value "thin" ] [ text "Thin "] 
                     , option [ value "light" ] [ text "Light "] 
                     , option [ value "regular" ] [ text "Regular "] 
@@ -90,45 +116,61 @@ view model =
                 , input
                     [ type_ "number"
                     , placeholder "Size"
+              
                     , value <| String.fromFloat model.size
                     , onInput SetSize
                     ] []
+                , button
+                    [ onClick Reset ]
+                    [ text "Reset" ]
                 ]
             , div
                 [ style "display" "flex"
                 , style "justify-content" "center"
                 , style "gap" "0.5em"
                 , style "font-size" fontSize
+                , style "transition" "font-size 200ms ease"
                 , style "padding" "0.25em"
                 , style "color" "darkolivegreen"
                 ]
-                [ Phosphor.campfire model.weight |> toHtml []
-                , Phosphor.cactus model.weight |> toHtml []
-                , Phosphor.beerBottle model.weight |> toHtml []
+                [ Phosphor.campfire model.weight |> withClass "grid-item" |> toHtml []
+                , Phosphor.cactus model.weight |> withClass "grid-item" |> toHtml []
+                , Phosphor.beerBottle model.weight |> withClass "grid-item" |> toHtml []
+                -- [ Phosphor.bookBookmark model.weight |> toHtml []
+                -- , Phosphor.cloudMoon model.weight |> toHtml []
+                -- , Phosphor.flask model.weight |> toHtml []
                 ]
             , div
                 [ style "display" "flex"
                 , style "justify-content" "center"
                 , style "gap" "0.5em"
                 , style "font-size" fontSize
+                , style "transition" "font-size 200ms ease"
                 , style "padding" "0.25em"
                 , style "color" "palevioletred"
                 ]
-                [ Phosphor.flyingSaucer model.weight |> toHtml []
-                , Phosphor.handEye model.weight |> toHtml []
-                , Phosphor.skull model.weight |> toHtml []
+                [ Phosphor.flyingSaucer model.weight |> withClass "grid-item" |> toHtml []
+                , Phosphor.handEye model.weight |> withClass "grid-item" |> toHtml []
+                , Phosphor.skull model.weight |> withClass "grid-item" |> toHtml []
+                -- [ Phosphor.mountains model.weight |> toHtml []
+                -- , Phosphor.package model.weight |> toHtml []
+                -- , Phosphor.snowflake model.weight |> toHtml []
                 ]
             , div
                 [ style "display" "flex"
                 , style "justify-content" "center"
                 , style "gap" "0.5em"
                 , style "font-size" fontSize
+                , style "transition" "font-size 200ms ease"
                 , style "padding" "0.25em"
                 , style "color" "steelblue"
                 ]
-                [ Phosphor.gameController model.weight |> toHtml []
-                , Phosphor.sword model.weight |> toHtml []
-                , Phosphor.strategy model.weight |> toHtml []
+                [ Phosphor.gameController model.weight |> withClass "grid-item" |> toHtml []
+                , Phosphor.sword model.weight |> withClass "grid-item" |> toHtml []
+                , Phosphor.strategy model.weight |> withClass "grid-item" |> toHtml []
+                -- [ Phosphor.sunglasses model.weight |> toHtml []
+                -- , Phosphor.tShirt model.weight |> toHtml []
+                -- , Phosphor.spiral model.weight |> toHtml []
                 ]
             ]
 
@@ -159,19 +201,24 @@ css =
     body {
         background-color: whitesmoke;
         overflow: hidden;
+        font-family: monospace;
     }
 
-    .ph-icon {
+    h2 {
+        color: darkgrey;
+    }
+
+    .grid-item {
         cursor: pointer;
         transition: transform 150ms ease, filter 200ms ease;
     }
 
-    .ph-icon:hover {
+    .grid-item:hover {
         transform: scale(1.25);
         filter: drop-shadow(0 0 32px currentColor);
     }
 
-    .ph-icon:active {
+    .grid-item:active {
         transform: scale(1.4);
     }
     """
